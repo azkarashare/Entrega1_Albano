4e3cac84429131eb3cb9f708c0d3621ad15ba505
azkarashare <89803587+azkarashare@users.noreply.github.com>
2022-09-03 19:36:16 -0300

    Importo modelos en admin y agrego fecha nac a usuario

--- LibCatalogo/models.py
+++ LibCatalogo/models.py
@@ -12,21 +12,34 @@ class Libros(models.Model):
     sumario=models.TextField(max_length=120)
     idioma=models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.titulo+" "+self.autor
+
 class Autores(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     fecha_n=models.DateField(null=True, blank=True)
     fecha_d=models.DateField('Fallecido', null=True, blank=True)
 
+    def __str__(self):
+        return self.nombre+" "+self.apellido
+
 class Genero(models.Model):
     nombre_g=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_g
 
 class Usuarios(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
+    fecha_n=models.DateField(null=True, blank=True)
     alias=models.CharField(max_length=50)
     correo=models.EmailField()
     contrasenia=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre+" "+self.apellido
 
 
 
